e98c4cdf1c33ffb518366ddfe74e4a3f10b4c0f5
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-04-30 15:25:20 +0100

    Fix logging to print whole message and * update README with example usage * change initilization so that ${projectDir} does not require a '/' at the end * fix broken toupper/tolower * add common logging methods to the init.sh * fix the 'fatal' logging message so that it fails with an exit not a kill
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- init.sh
+++ init.sh
@@ -8,8 +8,22 @@ main(){
 	)
 
 	for file in ${__files[@]}; do
-		source ${projectDir}bash-toolbox/lib/${file}
+		source ${projectDir}/bash-toolbox/lib/${file}
 	done
+
+	: '
+	Log info(), warning(), error(), etc from a subshell so that this
+	can be used within functions that return output.  In other words
+	the subshell ensures that the info,warning,error message does not dirty
+	the function output, the "echo"
+	'
+	include logger.Logger
+	debug()   { ( Logger log debug "${@}" ) }
+	info()    { ( Logger log info "${@}" ) }
+	warning() { ( Logger log warning "${@}" ) }
+	error()   { ( Logger log error "${@}" ) }
+	success() { ( Logger log success "${@}" ) }
+	fatal()   { ( Logger log fatal "${@}" ) }
 }
 
 main
